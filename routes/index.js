f4a436f85430f10a6fa945e09a28623ca6c30515
dadembii <dadembii@gmail.com>
2018-12-20 13:50:14 +0900

    login m #4

--- routes/index.js
+++ routes/index.js
@@ -61,6 +61,7 @@ module.exports = function(app, connectionPool) {
 
     app.post('/login', (req, res) => {
         /* 로그인시 Error 처리를 위한 route */
+        consol.log(req.error);
         res.redirect('/');
     });
 
@@ -78,6 +79,7 @@ module.exports = function(app, connectionPool) {
                         req.user = rows;
                         next();
                     }else {
+                        req.error = "nouser";
                         next('route');
                     }
                 }
